1c3d1204e1d2640ad6ad2714036193135a0f2a93
Tianyu Liang <tianyu@wireless-10-146-144-194.public.utexas.edu>
2018-02-08 18:43:10 -0600

    creation

--- Flix/NowPlayingViewController.swift
+++ Flix/NowPlayingViewController.swift
@@ -15,6 +15,9 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self;
+        
+        tableView.rowHeight = 180
+
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!;
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10);
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main);
