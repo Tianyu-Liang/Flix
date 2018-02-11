447f054a0cc5d9e2a605961632d195f1a6cdb3b8
Tianyu Liang <tianyu@Tianyus-MacBook-Pro.local>
2018-02-11 11:18:27 -0600

    newest

--- Flix/NowPlayingViewController.swift
+++ Flix/NowPlayingViewController.swift
@@ -7,17 +7,35 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class NowPlayingViewController: UIViewController, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
+    var movies: [[String: Any]] = [];
+    var refreshControl: UIRefreshControl!
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad();
+        refreshControl = UIRefreshControl();
+        refreshControl.addTarget(self, action: #selector (NowPlayingViewController.didPullToRefresh(_:)), for: .valueChanged)
+        tableView.insertSubview(refreshControl, at: 0);
         tableView.dataSource = self;
         
-        tableView.rowHeight = 180
-
+        tableView.rowHeight = 180;
+        fetchMovies();
+    }
+    
+    @objc func didPullToRefresh(_ refreshControl: UIRefreshControl){
+        activityIndicator.startAnimating();
+        fetchMovies();
+        activityIndicator.stopAnimating();
+    }
+    
+    func fetchMovies(){
+        
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!;
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10);
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main);
@@ -27,23 +45,30 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
                 print(error.localizedDescription);
             }else if let data = data{
                 let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any];
-                print(dataDictionary);
                 let movies = dataDictionary["results"] as! [[String: Any]];
-                for movie in movies{
-                    let title = movie["title"] as! String;
-                    print(title);
-                }
+                self.movies = movies;
+                self.tableView.reloadData();
+                self.refreshControl.endRefreshing();
             }
         }
         task.resume();
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10;
+        return movies.count;
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath);
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell;
+        let movie = movies[indexPath.row];
+        let title = movie["title"] as! String;
+        let overview = movie["overview"] as! String;
+        cell.titleLabel.text = title;
+        cell.overviewLabel.text = overview;
+        let posterPathString = movie["poster_path"] as! String;
+        let baseURLString = "https://image.tmdb.org/t/p/w500";
+        let posterURL = URL(string: baseURLString + posterPathString);
+        cell.posterImageView.af_setImage(withURL: posterURL!);
         return cell;
     }
 
